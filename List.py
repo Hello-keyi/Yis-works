d2808af4e1fa8ceee6a834f5db3522b7b3ea09a2
yi <13692959211.com>
2026-06-13 18:30:55 +0800

    学一点点列表

---
+++ List.py
@@ -0,0 +1,24 @@
+num = [1,2,3,4,5]
+print(num)
+print(num[2])
+pep = ['a','b','c','d','e','f','g','h','i','j']
+print(pep[4])
+print(pep[3].upper())
+print(num[3]+num[3])
+print(f"{num[3]} = 4")
+num [2] = 2
+print(num)
+num [2] = 3
+num.append(6)
+print(num)
+del num [0]
+print(num)
+num.append(7)
+print(num)
+x = num.pop(2)
+print(num)
+print(x)
+y = 7
+num.remove(y)
+print(num)
+print(y)
