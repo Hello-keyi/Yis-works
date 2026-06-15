34fe39dca4633624eea93c78cbf59c267b51d71e
yi <13692959211.com>
2026-06-15 17:28:13 +0800

    学习def语法2026.6.15

---
+++ Def.py
@@ -0,0 +1,34 @@
+def great_user ():
+    print("Hello")
+
+great_user()
+
+
+
+
+def great_username(user_name):
+    print(f"Hello {user_name.title()}!")
+
+great_username("li hua")
+
+
+
+
+def position (x,y):
+    print(f"Here is ({x},{y})")
+
+print(position(2,4))
+print(position(23,42))
+print(position(y = 2,x = 6))
+
+
+
+
+def players (name,job ="programer"):
+    print(f"Hello {name},your job is {job}")
+
+players("jack","doctor")
+
+
+
+
