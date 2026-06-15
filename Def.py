0a17700ca796c02bdce315a9615bee5adecec47e
yi <13692959211.com>
2026-06-15 21:15:34 +0800

    学习def函数2026.6.15

--- Def.py
+++ Def.py
@@ -1,34 +1,67 @@
 def great_user ():
     print("Hello")
-
 great_user()
-
 
 
 
 def great_username(user_name):
     print(f"Hello {user_name.title()}!")
-
 great_username("li hua")
-
-
-
-
-def position (x,y):
-    print(f"Here is ({x},{y})")
-
-print(position(2,4))
-print(position(23,42))
-print(position(y = 2,x = 6))
-
 
 
 
 def players (name,job ="programer"):
     print(f"Hello {name},your job is {job}")
-
 players("jack","doctor")
 
+
+
+def position (x,y,z=4,a=3):
+    print(x,y,z,a)
+position(1,2,"",4)
+
+
+
+def dic (first_name , last_name , age = None):
+    person = {"first":first_name,
+              "last":last_name,
+              "age":age
+              }
+    return person
+marry = dic("marry","poter",)
+print(marry)
+"""
+
+
+def get_fullname(f_name,l_name):
+    fullname = f"{f_name} {l_name}".title()
+    return fullname
+while True:
+    print("Tell me what is your name,"
+          "and I will back to you\n"
+          "If you whant to quit it"
+          ",please enter quit.")
+    f_name = input("What's your first name?")
+    f = f_name.title()
+    if f == "Quit":
+        print("Bye")
+        break
+    l_name = input("What's your last name?")
+    l = l_name.title()
+    if l == "Quit":
+        print("Bye")
+        break
+    fullname = get_fullname(f_name,l_name)
+    print(f"Hello {fullname}")
+
+"""
+
+def greet_users (names):
+    for name in names:
+        mg = f"Hello {name}"
+        print(mg)
+usernames = ["amili","bob","max","lily"]
+greet_users(usernames)
 
 
 
