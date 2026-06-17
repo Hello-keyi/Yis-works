8d2a9e29b1899aa4da43d0f706ba5b09854d28ed
yi <13692959211.com>
2026-06-17 15:57:01 +0800

    学class2026.6.17

--- Class.py
+++ Class.py
@@ -53,18 +53,35 @@ class Car :
         self.odometer_reading += miles
 
 
+class Battery:
+    
+    def __init__(self,battery_size = 40):
+        self.battery_size = battery_size
+
+    def descrube_battery(self):
+        print(f"this car has a {self.battery_size}-kwh battery.")
+
+    def get_range(self):
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+        print(f"this car can go about {range} mile on a full charge")
+
+
 class ElectricCar(Car):
 
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
-        self.battery_size = 40
+        self.battery = Battery()
 
     def describe_battery(self):
-        print(f"the car has a {self.battery_size}-kwh battery.")
+        print(f"the car has a {self.battery}-kwh battery.")
 
     def fill_gas_tank(self):
         print("this car don't have gas tank.")
-
+        
+        
 #-------------------------------------------------------------------------------------
 
 my_car = Car("audi","a4",2024)
@@ -83,4 +100,5 @@ my_car.read_odometer()
 my_leaf = ElectricCar("nissan","leaf",2024)
 print(my_leaf.get_descriptive_name())
 my_leaf.describe_battery()
+my_leaf.battery.get_range()
 
