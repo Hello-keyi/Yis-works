d2cae9236f32a3a2a873af9a54ef8af8c3e1458c
yi <13692959211.com>
2026-06-17 16:07:17 +0800

    学习import2026.6.17

--- Import.py
+++ Import.py
@@ -1,5 +1,10 @@
 from Def_forimport import get_fullname,hello
-
+import Class
+from random import randint,choice
 
 hello()
-
+my_car = Class.Car("audi","a4",2024)
+print(my_car.get_descriptive_name())
+print(randint(1,6))
+print(choice(["aaa","ddd","fff","ggg"]))
+#-----------------------------------------------------------------------------------------
